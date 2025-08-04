84966f28e5b449caf09b3ef45fb323f9c059e3f5
Bailey Chessum <bailey.chessum1@gmail.com>
2025-08-04 17:12:19 +1000

    feat: set up routing

---
+++ frontend/iou/src/views/LandingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function LandingPage() {
+
+  return (
+    <div className="py-0 p-0 min-h-screen w-full flex-col justify-center">
+      <div className="flex flex-row justify-center">
+        <h1 className="text-4xl font-light">IOU Calculator</h1>
+
+      </div>
+    </div>
+  )
+}
