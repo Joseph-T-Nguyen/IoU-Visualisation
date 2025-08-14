8d636327bb20fcc9b8e5d3a408e7f914457b0ec2
Danial <108564248+danielandinh@users.noreply.github.com>
2025-08-14 15:13:33 +1000

    Add WorkspacesPage and route to App
    added workspace page to localhost/workspaces
    
    #12

--- frontend/iou/src/App.tsx
+++ frontend/iou/src/App.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router";
 import LandingPage from "@/views/LandingPage/LandingPage.tsx"
+import WorkspacesPage from "@/views/WorkspacesPage/WorkspacesPage.tsx"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/workspaces" element={<WorkspacesPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/iou/src/views/WorkspacesPage/WorkspacesPage.tsx
@@ -0,0 +1,13 @@
+export default function WorkspacesPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      {/* Header will go here */}
+      
+      {/* Main content will go here */}
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <h1>Workspaces Page</h1>
+        <p>This is where your workspace content will go</p>
+      </div>
+    </div>
+  );
+}
