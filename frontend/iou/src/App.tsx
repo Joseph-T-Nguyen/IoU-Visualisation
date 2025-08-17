c01c567e2fafbd659d52697329f3d254e3a4fa3e
Joseph Nguyen <joseph.t.nguyen99@gmail.com>
2025-08-17 19:09:40 +1000

    Add /workspaces/new route for CreateNewWorkspacePage component

--- frontend/iou/src/App.tsx
+++ frontend/iou/src/App.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router";
 import LandingPage from "@/views/LandingPage/LandingPage.tsx"
+import CreateNewWorkspacePage from "./views/CreateNewWorkspacePage/CreateNewWorkspacePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/workspaces/new" element={<CreateNewWorkspacePage />} />
       </Routes>
     </BrowserRouter>
   );
