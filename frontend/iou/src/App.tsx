0da8677736c86cbe5443907563eb84770c526369
Joseph Nguyen <joseph.t.nguyen99@gmail.com>
2025-08-17 18:42:23 +1000

    Sign Up Page routing and structure, no form validation

--- frontend/iou/src/App.tsx
+++ frontend/iou/src/App.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router";
 import LandingPage from "@/views/LandingPage/LandingPage.tsx"
+import SignUpPage from "./views/SignUpPage/SignUpPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
