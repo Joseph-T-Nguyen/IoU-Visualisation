172b32141d79c7d30991eda014f6528a231e5fde
Bailey Chessum <bche0080@student.monash.edu>
2025-08-17 15:29:39 +1000

    feat: create useWorkspace hook to get workspace metadata

---
+++ frontend/iou/src/hooks/workspace/useWorkspace.ts
@@ -0,0 +1,16 @@
+import {useWorkspaceStore} from "@/hooks/workspace/useWorkspaceStore.ts";
+import {useShallow} from "zustand/react/shallow";
+import type {WorkspaceMetadata} from "@/hooks/workspace/workspaceTypes.ts";
+
+/**
+ * Hook to get metadata about a workspace
+ */
+function useWorkspace() {
+  const store: WorkspaceMetadata = useWorkspaceStore(useShallow((state) => ({
+    name: state.name,
+  })));
+
+  return store;
+}
+
+export default useWorkspace;
