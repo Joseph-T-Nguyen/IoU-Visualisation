387dc0ebe7ab24e19280d9e39069a29d46d9a838
Bailey Chessum <bche0080@student.monash.edu>
2025-08-18 13:18:31 +1000

    chore: removed unused hover callbacks from InstancedVertexSpheres
    They are no longer useful, and just serve to confuse people unfamiliar with the file.

--- frontend/iou/src/components/three/shape/InstancedVertexSpheres.tsx
+++ frontend/iou/src/components/three/shape/InstancedVertexSpheres.tsx
@@ -24,8 +24,7 @@ export default function InstancedVertexSpheres(props: InstancedVertexSpheresProp
   const radius = props.radius ?? 0.0625;
   const color = props.color ?? "white";
 
-  const [directHoveredId, setDirectHoveredId] = useState<number | null>(null);
-  const hoveredIds = props.hoveredIds ?? (directHoveredId === null ? [] : [directHoveredId]);
+  const hoveredIds = props.hoveredIds ?? [];
 
   // Precompute instance matrices
   const matrices = useMemo(() => {
@@ -34,7 +33,7 @@ export default function InstancedVertexSpheres(props: InstancedVertexSpheresProp
       m.setPosition(x, y, z);
       return m;
     });
-  }, [positions, meshRef]);
+  }, [positions, meshRef.current]);
 
   const hoveredMatrices = useMemo(() => {
     return hoveredIds.map((id: number) => matrices[id])
@@ -63,52 +62,20 @@ export default function InstancedVertexSpheres(props: InstancedVertexSpheresProp
     hoveredOuterMeshRef.current.instanceMatrix.needsUpdate = true;
   }, [hoveredMatrices]);
 
-  const onPointerMove = useCallback((event: ThreeEvent<PointerEvent>) => {
-    const newId = event.instanceId ?? null;
-
-    if (newId === directHoveredId)
-      return;
-
-    if (directHoveredId !== null)
-      props.onVertexPointerOut?.(directHoveredId, event);
-    if (newId !== null)
-      props.onVertexPointerEnter?.(newId, event);
-
-    setDirectHoveredId(newId);
-  }, [directHoveredId, props.onVertexPointerOut, props.onVertexPointerEnter]);
-
-  const onPointerOut = useCallback((event: ThreeEvent<PointerEvent>) => {
-    if (directHoveredId !== null)
-      props.onVertexPointerOut?.(directHoveredId, event);
-
-    setDirectHoveredId(null)
-  }, [directHoveredId, props.onVertexPointerOut, props.onVertexPointerEnter]);
-
-  const onClick = useCallback((event: ThreeEvent<PointerEvent>) => {
-    console.log('Clicked vertex instance', event.instanceId);
-    props.onClick?.()
-  }, [props.onClick]);
-
   return (
-    <group
-      onPointerOut={onPointerOut}
-    >
+    <>
       <instancedMesh
         ref={meshRef}
         args={[undefined, undefined, positions.length]}
-        onPointerMove={onPointerMove}
-        onClick={onClick}
       >
         {/* Base sphere geometry */}
         <sphereGeometry args={[radius, 8, 8]} />
         <meshBasicMaterial color={color} toneMapped={false}/>
       </instancedMesh>
-      {/* Optional: inverted outer sphere for hovered instance */}
+
       <instancedMesh
         ref={hoveredInnerMeshRef}
         args={[undefined, undefined, hoveredMatrices.length]}
-        onPointerMove={onPointerMove}
-        onClick={onClick}
       >
         <sphereGeometry args={[radius, 8, 8]} />
         <meshBasicMaterial color="white" toneMapped={false}/>
@@ -116,12 +83,10 @@ export default function InstancedVertexSpheres(props: InstancedVertexSpheresProp
       <instancedMesh
         ref={hoveredOuterMeshRef}
         args={[undefined, undefined, hoveredMatrices.length]}
-        onPointerMove={onPointerMove}
-        onClick={onClick}
       >
         <sphereGeometry args={[radius * 1.5, 8, 8]} />
         <meshBasicMaterial color="#00D3F2" side={BackSide} toneMapped={false}/>
       </instancedMesh>
-    </group>
+    </>
   );
 }
