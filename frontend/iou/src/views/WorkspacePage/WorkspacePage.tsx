65b23207bb7b2cfa25dafa087cc1bbae9659eec9
Chhay Irseng <cirs0001@student.monash.edu>
2025-09-17 13:06:08 +1000

    feat: add AxisGizmo and CoordinateSystem components for enhanced 3D axis visualization

--- frontend/iou/src/views/WorkspacePage/WorkspacePage.tsx
+++ frontend/iou/src/views/WorkspacePage/WorkspacePage.tsx
@@ -1,6 +1,6 @@
 import FlexyCanvas from "@/components/shared/FlexyCanvas.tsx";
-import {Button} from "@/components/ui/button.tsx";
-import { LogOut } from "lucide-react"
+import { Button } from "@/components/ui/button.tsx";
+import { LogOut } from "lucide-react";
 import WorkspaceMenubar from "@/components/widgets/workspace/WorkspaceMenubar.tsx";
 import ContextSidebar from "@/components/widgets/workspace/ContextSidebar.tsx";
 import useDimensions from "@/hooks/workspace/useDimensions.ts";
@@ -11,16 +11,17 @@ import WorkspaceActionListener from "@/components/widgets/workspace/WorkspaceAct
 import VertexControls from "@/components/three/VertexControls.tsx";
 import WorkspaceCamera from "@/components/three/WorkspaceCamera.tsx";
 import WorkspaceGrid from "@/components/three/WorkspaceGrid.tsx";
-import {useEffect} from "react";
-import {AdaptiveEvents, Bvh} from "@react-three/drei";
+import { useEffect } from "react";
+import { AdaptiveEvents, Bvh } from "@react-three/drei";
 import useShapesStore from "@/hooks/workspace/stores/useShapesStore.ts";
+import CoordinateSystem from "@/components/three/CoordinateSystem.tsx";
 
 export default function WorkspacePage() {
   const [dimensions, setDimensions] = useDimensions();
 
   const shapeUUIDs = useShapeUUIDs();
 
-  const deselect = useShapesStore(s => s.deselect);
+  const deselect = useShapesStore((s) => s.deselect);
 
   // These are all the JSX elements used as an overlay on top of the 3d/2d view
   const overlay = (
@@ -29,7 +30,12 @@ export default function WorkspacePage() {
         <div className="grid grid-cols-[auto_auto_auto_auto] gap-3 w-fit">
           {/* Main view overlay */}
           <div>
-            <Button variant="outline" size="icon" className="size-8 pointer-events-auto w-9 h-9 cursor-pointer shadow-lg" asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="size-8 pointer-events-auto w-9 h-9 cursor-pointer shadow-lg"
+              asChild
+            >
               <a href="../">
                 <LogOut className="transform scale-x-[-1] " />
               </a>
@@ -40,18 +46,20 @@ export default function WorkspacePage() {
           </div>
           <div className="flex flex-col justify-center pointer-events-auto">
             <Button
-              size="icon" variant="outline" className="shadow-lg font-light text-md cursor-pointer"
+              size="icon"
+              variant="outline"
+              className="shadow-lg font-light text-md cursor-pointer"
               onClick={() => setDimensions(dimensions === "3d" ? "2d" : "3d")}
             >
               {dimensions?.toUpperCase() ?? "ERR"}
             </Button>
           </div>
           <div className="flex flex-col justify-center items-center">
-            <WorkspaceTitle/>
+            <WorkspaceTitle />
           </div>
         </div>
       </div>
-      <ContextSidebar className="min-w-64 overflow-y-scroll"/>
+      <ContextSidebar className="min-w-64 overflow-y-scroll" />
     </div>
   );
 
@@ -61,57 +69,60 @@ export default function WorkspacePage() {
       e.preventDefault();
     };
 
-    document.body.addEventListener('touchmove', preventTouch, { passive: false });
+    document.body.addEventListener("touchmove", preventTouch, {
+      passive: false,
+    });
 
     return () => {
-      document.body.removeEventListener('touchmove', preventTouch);
-    }
+      document.body.removeEventListener("touchmove", preventTouch);
+    };
   }, []);
 
-  return (<>
-    <WorkspaceActionListener />
-    <FlexyCanvas
-      /* min-h-[100dv] works better on mobile devices that h-screen */
-      className="w-screen min-h-[100dvh] overflow-clip overscroll-contain bg-secondary"
-      overlay={overlay}
-      onPointerMissed={() => {
-        deselect();
-      }}
-      onCreated={({ raycaster, camera }) => {
-        // Only see layers 0 and 1
-        raycaster.layers.set(0)
-        raycaster.layers.enable(1)
-        camera.layers.set(0)
-        camera.layers.enable(0)
-        camera.layers.enable(1)
-      }}
+  return (
+    <>
+      <WorkspaceActionListener />
+      <FlexyCanvas
+        /* min-h-[100dv] works better on mobile devices that h-screen */
+        className="w-screen min-h-[100dvh] overflow-clip overscroll-contain bg-secondary"
+        overlay={overlay}
+        onPointerMissed={() => {
+          deselect();
+        }}
+        onCreated={({ raycaster, camera }) => {
+          // Only see layers 0 and 1
+          raycaster.layers.set(0);
+          raycaster.layers.enable(1);
+          camera.layers.set(0);
+          camera.layers.enable(0);
+          camera.layers.enable(1);
+        }}
+      >
+        <WorkspaceGrid />
+        <AdaptiveEvents />
 
-    >
-      <WorkspaceGrid/>
-      <AdaptiveEvents />
+        <WorkspaceCamera />
 
+        <CoordinateSystem />
 
-      <WorkspaceCamera/>
+        <Bvh firstHitOnly>
+          <VertexControls />
 
-      <Bvh firstHitOnly>
+          {/* Add 3D content here: */}
 
-        <VertexControls/>
+          {/* Add every shape to the scene: */}
+          {shapeUUIDs.map((uuid: string) => (
+            <ShapeWidget uuid={uuid} key={uuid} />
+          ))}
+        </Bvh>
 
-        {/* Add 3D content here: */}
-
-        {/* Add every shape to the scene: */}
-        {shapeUUIDs.map((uuid: string) => (
-          <ShapeWidget uuid={uuid} key={uuid}/>
-        ))}
-      </Bvh>
-
-      <ambientLight intensity={0.25} color="#F1F5F9"/>
-      <directionalLight position={[1, 5, 2]} intensity={2} rotation={[45, 10, 0]} color="white" />
-
-    </FlexyCanvas>
-  </>);
+        <ambientLight intensity={0.25} color="#F1F5F9" />
+        <directionalLight
+          position={[1, 5, 2]}
+          intensity={2}
+          rotation={[45, 10, 0]}
+          color="white"
+        />
+      </FlexyCanvas>
+    </>
+  );
 }
-
-
-
-
