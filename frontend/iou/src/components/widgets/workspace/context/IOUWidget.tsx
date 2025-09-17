2d9fbf9d2fda8f8cf6db194e367fe3c187a337ad
Bailey Chessum <bailey.chessum1@gmail.com>
2025-09-17 22:21:04 +1000

    feat: made the IOU show as 0 when there is no intersection

--- frontend/iou/src/components/widgets/workspace/context/IOUWidget.tsx
+++ frontend/iou/src/components/widgets/workspace/context/IOUWidget.tsx
@@ -8,7 +8,7 @@ export default function IOUWidget() {
 
   return (
     <Card className="pointer-events-auto gap-1.5 px-0 shadow-lg justify-stretch pt-1.5 pb-3">
-      <CardContent className="px-5 mt-1.5 flex flex-row gap-5 text-xl items-center h-7.5">
+      <CardContent className="px-5 mt-1.5 flex flex-row gap-5 text-2xl items-center h-7.5">
         <div className="">
           IOU
 
@@ -18,7 +18,7 @@ export default function IOUWidget() {
         />
 
         <div className="grow font-mono font-light">
-          {iou?.toFixed(10) ?? "???"}
+          {(iou ?? 0).toFixed(10)}
         </div>
       </CardContent>
     </Card>
