773dfc2b9dc28dd9a4bab49c3dcee1cb07932c6b
Bailey Chessum <bailey.chessum1@gmail.com>
2025-09-22 17:48:20 +1000

    fix: reduce re-renders from useShapeUUIDs.tsx

--- frontend/iou/src/hooks/workspace/useShapeUUIDs.tsx
+++ frontend/iou/src/hooks/workspace/useShapeUUIDs.tsx
@@ -1,12 +1,14 @@
 import useShapesStore from "@/hooks/workspace/stores/useShapesStore.ts";
-import { useMemo } from "react";
+import { useShallow } from 'zustand/shallow';
 
 /**
  * A hook to get the UUIDs of all shapes
  */
 export default function useShapeUUIDs() {
-  const shapes = useShapesStore(s => s.shapes);
-  const shapeUUIDs = useMemo<string[]>(() => Object.keys(shapes), [shapes]);
+  // const shapeUUIDs = []
+  const shapeUUIDs = useShapesStore(useShallow(
+    s => Object.keys(s.shapes)
+  ));
 
   return shapeUUIDs;
 }
